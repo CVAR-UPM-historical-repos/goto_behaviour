0d61c811d0e32d8cd8946f40f1ac286ddc0c4427
miguel <miguelgranero99@gmail.com>
2022-09-07 16:23:19 +0200

    fixed yaw_to_goal constraint to +-180º

--- goto_plugin_base/include/goto_plugin_base/goto_base.hpp
+++ goto_plugin_base/include/goto_plugin_base/goto_base.hpp
@@ -54,6 +54,8 @@
 #include <Eigen/Dense>
 #include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 
+#define PI 3.1416
+
 namespace goto_base
 {
     class GotoBase
@@ -122,10 +124,14 @@ namespace goto_base
 
             this->actual_distance_to_goal_ = (actual_position_ - desired_position_).norm();
 
-            float yaw_to_goal = fabs(as2::FrameUtils::getYawFromQuaternion(actual_q_) - desired_yaw_);
-            while(yaw_to_goal > 3.1416/2)
-                yaw_to_goal -= 3.1416;
-            this->actual_yaw_to_goal_ = yaw_to_goal;
+            // this->actual_yaw_to_goal_ = as2::FrameUtils::getYawFromQuaternion(actual_q_) - desired_yaw_;
+             
+            if( as2::FrameUtils::getYawFromQuaternion(actual_q_) - desired_yaw_ > PI/2.0)
+                this->actual_yaw_to_goal_ = as2::FrameUtils::getYawFromQuaternion(actual_q_) - desired_yaw_ - PI;
+            else if( as2::FrameUtils::getYawFromQuaternion(actual_q_) - desired_yaw_ < -PI/2.0)
+                this->actual_yaw_to_goal_ = as2::FrameUtils::getYawFromQuaternion(actual_q_) - desired_yaw_ + PI;
+            else
+                this->actual_yaw_to_goal_ = as2::FrameUtils::getYawFromQuaternion(actual_q_) - desired_yaw_;
 
             this->actual_speed_ = Eigen::Vector3d(twist_msg->twist.linear.x,
                                                   twist_msg->twist.linear.y,
